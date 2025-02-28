bede0ed5edae74bde08ffb2ff94e73ce90a7a832
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-02-28 15:10:49 +0100

    Calls Layout component in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 }
 
 export default App;
