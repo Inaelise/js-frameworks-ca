b00c8e42985acbb8bc3e613d0f667c68575131a8
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-03-03 10:26:46 +0100

    Gets cart state, calculates total items and displays number of items in cart

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,10 +1,16 @@
 import { ShoppingCart } from "lucide-react";
 import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
+import { useContext } from "react";
 
 export default function CartIcon() {
+  const { cart } = useContext(CartContext);
+  const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <Link to="/checkout">
       <ShoppingCart />
+      {cartCount > 0 && <span>{cartCount}</span>}
     </Link>
   );
 }
