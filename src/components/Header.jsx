69f0f2796ed3603ec2a59055d9d7586f06465529
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-03-05 18:11:54 +0100

    Add css module to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import Nav from "./Nav";
 import CartIcon from "./CartIcon";
+import styles from "../css/Header.module.css";
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <img className="logo" src="/images/variety-logo.png" alt="Variety logo" />
       <Nav />
       <CartIcon />
