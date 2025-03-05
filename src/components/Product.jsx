ef8b7214cef1b3b6bffe9d3653404e7ebcca341e
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-03-05 16:59:11 +0100

    Removes button link and adds link to whole component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,16 +3,13 @@ import ProductPrice from "./ProductPrice";
 
 export default function Product(props) {
   return (
-    <div>
+    <Link to={`/product/${props.id}`}>
       <img src={props.img.url} alt={props.img.alt} />
       <h2>{props.title}</h2>
       <ProductPrice
         price={props.price}
         discountedPrice={props.discountedPrice}
       />
-      <Link to={`/product/${props.id}`}>
-        <button>View</button>
-      </Link>
-    </div>
+    </Link>
   );
 }
