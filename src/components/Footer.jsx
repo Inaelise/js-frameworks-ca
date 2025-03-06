3f1da6c64c3deb10335d136f79a978c69da0b989
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-03-06 13:49:19 +0100

    Adds styling to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import styles from "../css/Footer.module.css";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>Copyright © 2025 Variety</p>
+    <footer className={styles.footer}>
+      <p className={styles.footerText}>Copyright © 2025 Variety</p>
     </footer>
   );
 }
