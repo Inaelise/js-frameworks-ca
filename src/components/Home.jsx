1ca37afb44a571b6c9a1b4abb62e18b43f6efe3d
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-03-06 18:55:39 +0100

    Adds some styling to product list and items

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import Product from "./Product";
 import SearchBar from "./SearchBar";
+import styles from "../css/Home.module.css";
 
 export default function Home({ items, onSearch }) {
   return (
@@ -8,23 +9,25 @@ export default function Home({ items, onSearch }) {
         <div>
           <SearchBar onSearch={onSearch} items={items} />
         </div>
-        {items.length > 0 ? (
-          items.map((item) => (
-            <li key={item.id}>
-              <Product
-                id={item.id}
-                img={item.image}
-                title={item.title}
-                body={item.description}
-                price={item.price}
-                discountedPrice={item.discountedPrice}
-                rating={item.rating}
-              />
-            </li>
-          ))
-        ) : (
-          <p>No products found.</p>
-        )}
+        <div className={styles.productList}>
+          {items.length > 0 ? (
+            items.map((item) => (
+              <li key={item.id} className={styles.productItem}>
+                <Product
+                  id={item.id}
+                  img={item.image}
+                  title={item.title}
+                  body={item.description}
+                  price={item.price}
+                  discountedPrice={item.discountedPrice}
+                  rating={item.rating}
+                />
+              </li>
+            ))
+          ) : (
+            <p>No products found.</p>
+          )}
+        </div>
       </section>
     </main>
   );
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 import ProductPrice from "./ProductPrice";
 import StarRating from "./StarRating";
+import styles from "../css/Home.module.css";
 
 export default function Product(props) {
   return (
-    <Link to={`/product/${props.id}`}>
+    <Link
+      to={`/product/${props.id}`}
+      className={styles.product}
+      title="View Product"
+    >
       <img src={props.img.url} alt={props.img.alt} />
       <h2>{props.title}</h2>
       <ProductPrice
