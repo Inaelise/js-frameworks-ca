34fcf8d1590f50c3b2189577e854a7ff228b4db7
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-03-06 18:42:15 +0100

    Adds home css module and adds styling to search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import { Search } from "lucide-react";
+import { X } from "lucide-react";
+import styles from "../css/Home.module.css";
 
 export default function SearchBar({ onSearch, items }) {
   const [query, setQuery] = useState("");
@@ -54,25 +56,44 @@ export default function SearchBar({ onSearch, items }) {
   }, []);
 
   return (
-    <div ref={searchRef}>
-      <Search />
-      <input
-        type="text"
-        value={query}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        placeholder="Search..."
-      />
-      {query && <button onClick={clearSearch}>x</button>}
-      {suggestions.length > 0 && (
-        <ul>
-          {suggestions.map((item) => (
-            <li key={item.id} onClick={() => handleSelect(item.title)}>
-              {item.title}
-            </li>
-          ))}
-        </ul>
-      )}
+    <div ref={searchRef} className={styles.searchContainer}>
+      <div className={styles.searchBarContainer}>
+        <span className={styles.iconContainer}>
+          <Search size={20} strokeWidth={3} className={styles.searchIcon} />
+        </span>
+        <input
+          className={styles.searchBar}
+          type="text"
+          value={query}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          placeholder="Search..."
+        />
+        {query && (
+          <button
+            className={styles.searchClear}
+            onClick={clearSearch}
+            title="Clear search"
+          >
+            <X size={20} />
+          </button>
+        )}
+      </div>
+      <div className={styles.suggestionsContainer}>
+        {suggestions.length > 0 && (
+          <ul className={styles.suggestionsContainer}>
+            {suggestions.map((item) => (
+              <li
+                className={styles.suggestion}
+                key={item.id}
+                onClick={() => handleSelect(item.title)}
+              >
+                {item.title}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
