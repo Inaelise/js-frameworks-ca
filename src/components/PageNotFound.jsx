145adc8ffb868b5ebdcd6eeb3508814bb4c9a64a
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-03-07 23:27:27 +0100

    Adds elements and styling to page not found page

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,3 +1,21 @@
+import styles from "../css/PageNotFound.module.css";
+import { NavLink } from "react-router-dom";
+import { CircleAlert } from "lucide-react";
+
 export default function PageNotFound() {
-  return <p>Page not found</p>;
+  return (
+    <main className={styles.noPageMain}>
+      <div className={styles.noPageContainer}>
+        <CircleAlert size={38} />
+        <h1>Page not found</h1>
+        <p>The page you're trying to access doesn't exist.</p>
+        <p>Try going back to the home page.</p>
+        <NavLink to="/">
+          <button className={styles.noPageBtn} title="Go to home">
+            Go to home
+          </button>
+        </NavLink>
+      </div>
+    </main>
+  );
 }
