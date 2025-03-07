10d08481be418c2f59bc41c6710fd0b3b94853f8
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-03-07 13:44:40 +0100

    Adds elements and styling

--- src/components/CheckoutSuccessPage.jsx
+++ src/components/CheckoutSuccessPage.jsx
@@ -1,3 +1,27 @@
+import { NavLink } from "react-router-dom";
+import { Check } from "lucide-react";
+import styles from "../css/CheckoutSuccess.module.css";
+
 export default function CheckoutSuccessPage() {
-  return <h1>This is the checkout success page component</h1>;
+  return (
+    <main>
+      <div className={styles.checkoutSuccess}>
+        <div className={styles.checkoutSuccessBorder}>
+          <div className={styles.checkmarkContainer}>
+            <Check color="white" size={32} strokeWidth={1.5} />
+          </div>
+          <h1>Thank you for your purchase!</h1>
+          <p>
+            We've recieved your order and will begin processing it. Order
+            confirmation will be sent to your email.
+          </p>
+          <NavLink to="/">
+            <button className={styles.checkoutSuccessBtn} title="Go to home">
+              Back to home
+            </button>
+          </NavLink>
+        </div>
+      </div>
+    </main>
+  );
 }
